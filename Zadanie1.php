bedea625eff49875feff9d8ee0ec6b716fa19fdf
tobi23fmr <394-skc-2asu@mail.ru>
2024-08-23 22:54:50 +0300

    13

--- Zadanie1.php
+++ Zadanie1.php
@@ -13,20 +13,11 @@
                     echo '<div>';
                     if ($ba == date("Y")) {
                         echo '<div style="color:purple">';
-                    } else {
-                            echo '<div>';
-                            if ($ba == 2000) {
-                                $ba = 0;
-                                echo "Yappi";                        
-                            } else {
-                                echo '<div>';# code...
-                            }
                     }
-            }
+                    }
+            };
             echo $ba;
             echo '<br></div>';
-        };
-        ?>
-
+         ?>
     </body>    
 </html>
