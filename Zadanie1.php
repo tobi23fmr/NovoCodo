4c044f99441e1c399eabfa833c5fe82f327854fc
tobi23fmr <394-skc-2asu@mail.ru>
2024-08-24 00:42:19 +0300

    21

--- Zadanie1.php
+++ Zadanie1.php
@@ -6,18 +6,21 @@
     </head>
     <body> 
         <?php
+        $b = "Yappi";
         for ($ba = 1992; $ba <= date("Y"); $ba++){
             if ($ba == 1992) {
-                echo '<div style="color:purple ">';
-            } else {
-                    echo '<div>';
-                    if ($ba == date("Y")) {
-                        echo '<div style="color:purple">';
-                    }
-                    }
+                echo '<div style="color:blue">';
+            };
+            if ($ba == date("Y")) {
+                echo '<div style="color:blue">';
+            };
+            if ($ba == 2000) {
+                echo str_replace('2000' , 'Yappi' , $ba);
             };
             echo $ba;
             echo '<br></div>';
+        };
+        
          ?>
     </body>    
 </html>
