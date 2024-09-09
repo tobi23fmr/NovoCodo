d6c18cbe9d9f4cb48116bbe9fd9a2613c396500d
tobi23fmr <394-skc-2asu@mail.ru>
2024-09-10 00:08:34 +0300

    123

--- local/templates/zoopriut/header.php
+++ local/templates/zoopriut/header.php
@@ -4,7 +4,7 @@
     <?$APPLICATION->ShowHead();?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ZooПриют | Главная страница</title>
+    <title><?$APPLICATION->ShowTitle()?></title>
     <link rel="stylesheet" href="<? echo SITE_TEMPLATE_PATH; ?>/css/style.css">
     <script src="<? echo SITE_TEMPLATE_PATH; ?>/JS/jquery-3.7.1.min.js"></script>
     <script src="<? echo SITE_TEMPLATE_PATH; ?>/JS/my.js"></script>
@@ -15,11 +15,11 @@
         <tr>
             <td width="300"><center>ZooПриют О_о</center></td>
             <td>
-                <center><a href="/ZooPriut/index.php">Главная</a> / 
-                <a href="/ZooPriut/News/index.php">Новости</a> / 
-                <a href="/ZooPriut/Animals/index.html">Наши зверята</a> / 
-                <a href="/ZooPriut/Pitomniki/index.php">Питомники</a> / 
-                <a href="/ZooPriut/About/index.php">О приюте</a> </center>
+                <center><a href="/index.php">Главная</a> / 
+                <a href="/News/index.php">Новости</a> / 
+                <a href="/nashi-zveryata/">Наши зверята</a> / 
+                <a href="/Pitomniki/index.php">Питомники</a> / 
+                <a href="/About/index.php">О приюте</a> </center>
             </td>
             <td width="300"><center>Вход / Регистрация</center></td>
         </tr>
